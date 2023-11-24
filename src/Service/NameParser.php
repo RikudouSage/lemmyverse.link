3bd7107234620d84a2d9ebd88ef5817b3ef4affb
Dominik Chrastecky <dominik@chrastecky.cz>
2023-11-24 10:33:37 +0100

    Allow uppercase letters in a link

--- src/Service/NameParser.php
+++ src/Service/NameParser.php
@@ -9,7 +9,7 @@ final class NameParser
 {
     public function parse(string $identifier): ParsedName
     {
-        $regex = '/^(?<CommunityOrUserName>[a-z0-9_]+)@(?<Instance>[a-zA-Z0-9][a-zA-Z0-9-.]{0,61}[a-zA-Z0-9])$/';
+        $regex = '/^(?<CommunityOrUserName>[a-zA-Z0-9_]+)@(?<Instance>[a-zA-Z0-9][a-zA-Z0-9-.]{0,61}[a-zA-Z0-9])$/';
         if (!preg_match($regex, $identifier, $matches)) {
             throw new InvalidArgumentException("Invalid community: {$identifier}");
         }
