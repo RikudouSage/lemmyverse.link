31457e79937a60e881b7d49b50eee34bc2e6d252
Dominik Chrastecky <dominik@chrastecky.cz>
2025-04-12 17:03:22 +0200

    Fix code style

--- src/Service/ActivityPubResolver.php
+++ src/Service/ActivityPubResolver.php
@@ -5,7 +5,6 @@ namespace App\Service;
 use App\Dto\ActivityPubItem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
